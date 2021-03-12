f22154feb207e7fe4539fc35c851c82cf46c74f6
eandualem <eandualem@gmail.com>
2021-03-12 16:09:07 +0300

    added product page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,24 @@
+import 'package:ethio_shoppers/ui/views/home/home_page.dart';
+import 'package:ethio_shoppers/ui/views/products/products_overview_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  runApp(EthioShoppers());
 }
+
+class EthioShoppers extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "EthioShoppers",
+      theme: ThemeData(
+
+      ),
+      routes: {
+        '/': (_) => ProductsOverviewScreen(),
+      }
+
+    );
+  }
+}
+
